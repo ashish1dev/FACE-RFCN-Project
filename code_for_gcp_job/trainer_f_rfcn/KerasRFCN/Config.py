8a382064b27febfbf3fefb2881eb9aad5f0fe021
Ashish Kumar <akumar46@buffalo.edu>
2019-05-06 06:11:28 -0400

    changes

--- code_for_gcp_job/trainer_f_rfcn/KerasRFCN/Config.py
+++ code_for_gcp_job/trainer_f_rfcn/KerasRFCN/Config.py
@@ -57,7 +57,8 @@ class Config(object):
     BACKBONE_STRIDES = [4, 8, 16, 32, 64]
 
     # Number of classification classes (including background)
-    NUM_CLASSES = 1  # Override in sub-classes
+    # NUM_CLASSES = 1  # Override in sub-classes
+    NUM_CLASSES = 2  # Override in sub-classes
 
     # Length of square anchor side in pixels
     RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
@@ -165,6 +166,3 @@ class Config(object):
             if not a.startswith("__") and not callable(getattr(self, a)):
                 print("{:30} {}".format(a, getattr(self, a)))
         print("\n")
-
-
-
