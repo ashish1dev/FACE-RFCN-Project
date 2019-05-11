2b983e5fe916df94a1e0d0e31f40ccc13e717b8c
Ashish Kumar <akumar46@buffalo.edu>
2019-05-11 12:57:29 -0400

    changes

--- Fashion_Train.py
+++ Fashion_Train.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     print("Training on all layers")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=1,
+                epochs=25,
                 layers='all')
 
     # # Training - Stage 1
