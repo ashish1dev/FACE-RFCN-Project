ec7028aeaa3bedb99dfc433858a7ec6409e3ed9e
Ashish Kumar <akumar46@buffalo.edu>
2019-05-17 15:05:15 -0400

    changes

--- KerasRFCN/Losses.py
+++ KerasRFCN/Losses.py
@@ -115,7 +115,8 @@ def mrcnn_class_loss_graph(target_class_ids, pred_class_logits,
 
     # Computer loss mean. Use only predictions that contribute
     # to the loss to get a correct mean.
-    loss = tf.cast(loss, tf.float32)
+    # loss = tf.cast(loss, tf.float32)
+    loss = tf.to_float(loss)
     loss = tf.reduce_sum(loss) / tf.reduce_sum(pred_active)
     return loss
 
